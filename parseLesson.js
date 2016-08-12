2a5da89b3413e2e33e4752b827e95406a9d30d2a
shgtkshruch <shgtk.shruch@gmail.com>
2016-08-12 17:16:07 +0900

    Add parseLesson

---
+++ parseLesson.js
@@ -0,0 +1,19 @@
+const requestLesosn = require('./requestLesson');
+const cheerio = require('cheerio');
+
+module.exports = function (body, cb) {
+  const $ = cheerio.load(body);
+  const lesson = [];
+  $('#lessons_list > li').each(function (i, elem) {
+    const content = {};
+    content.name = $(this).find('> a').text();
+    content.time = $(this).find('> a + span').text();
+    const summaries = [];
+    $(this).find('> .lesson-summaries > li').each(function (i, elem) {
+      summaries.push($(this).text());
+    });
+    content.summaries = summaries;
+    lesson.push(content);
+  });
+  cb(lesson);
+};
---
+++ test/parseLesson.js
@@ -0,0 +1,15 @@
+const parseLesson = require('../parseLesson');
+const fs = require('fs');
+const assert = require('assert');
+const html = fs.readFileSync('test/fixture/lesson.html', 'utf8');
+
+describe('parseLesson', () => {
+  it('should return lesson data', (done) => {
+    parseLesson(html, (lesson) => {
+      assert(lesson[0].name.includes('HTMLとはなにか？'), true);
+      assert(lesson[0].time.includes('02:56'), true);
+      assert(lesson[0].summaries.includes('概要'), true);
+      done();
+    });
+  });
+});
