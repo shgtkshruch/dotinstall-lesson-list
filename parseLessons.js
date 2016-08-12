a480e626e49664632dfc826d7140c93d17aec9d8
shgtkshruch <shgtk.shruch@gmail.com>
2016-08-12 16:55:37 +0900

    Remove unnecessary variable

--- parseLessons.js
+++ parseLessons.js
@@ -1,12 +1,12 @@
 const cheerio = require('cheerio');
-const dotinstallUrl = 'http://dotinstall.com';
+const dotinstallUrl = '';
 
 module.exports = (body, cb) => {
   const $ = cheerio.load(body);
   const lessons = [];
   $('.all-packages-list a').each(function (i, elem) {
     const lessonName = $(this).text();
-    const lessonLink = dotinstallUrl + $(this).attr('href');
+    const lessonLink = 'http://dotinstall.com' + $(this).attr('href');
     lessons.push({lessonName, lessonLink});
   });
   cb(lessons);
